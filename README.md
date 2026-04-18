d44c1eb5eded5d5e8a5f8622e95f03fa4f969d6b
Alex <a.mas.bosch@gmail.com>
2026-04-18 14:33:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-dnd
+
+Simple react-hook abstraction over the native drag and drop interface. It handles adding/removing the event handlers and the neccesary logic for the provided ref to become draggable/droppable
---
+++ src/dnd.ts
@@ -0,0 +1,90 @@
+import { useEffect, useRef } from "react";
+
+export type OnDragHandler = (event: DragEvent) => void;
+export type UseDragConfig = {
+  onStart?: OnDragHandler;
+  onDragging?: OnDragHandler;
+  onDragOver?: OnDragHandler;
+  onDragEnter?: OnDragHandler;
+  onDragLeave?: OnDragHandler;
+  onDrop?: OnDragHandler;
+  droppable: boolean;
+  draggable: boolean;
+};
+export const useDnD = <RefType extends HTMLElement>({
+  onStart,
+  onDragging,
+  onDrop,
+  onDragOver,
+  onDragEnter,
+  onDragLeave,
+  droppable,
+  draggable,
+}: UseDragConfig) => {
+  const ref = useRef<RefType>(null);
+  useEffect(() => {
+    let onDragOverHandler: OnDragHandler | undefined = droppable
+      ? (e) => {
+          e.preventDefault();
+        }
+      : undefined;
+    if (onDragOver) {
+      if (droppable) {
+        onDragOverHandler = (e) => {
+          e.preventDefault();
+          return onDragOver(e);
+        };
+      } else {
+        onDragOverHandler = onDragOver;
+      }
+    }
+    if (ref.current) {
+      ref.current.draggable = draggable;
+
+      if (onStart) {
+        ref.current.addEventListener("dragstart", onStart);
+      }
+      if (onDragging) {
+        ref.current.addEventListener("drag", onDragging);
+      }
+      if (onDragEnter) {
+        ref.current.addEventListener("dragenter", onDragEnter);
+      }
+      if (onDragLeave) {
+        ref.current.addEventListener("dragleave", onDragLeave);
+      }
+
+      if (droppable && onDrop) {
+        ref.current.addEventListener("drop", onDrop);
+      }
+      if (onDragOverHandler) {
+        ref.current.addEventListener("dragover", onDragOverHandler);
+      }
+    }
+    return () => {
+      if (ref.current) {
+        if (draggable) {
+          ref.current.draggable = false;
+        }
+        if (onStart) {
+          ref.current.addEventListener("dragstart", onStart);
+        }
+        if (onDragging) {
+          ref.current.addEventListener("drag", onDragging);
+        }
+        if (onDragEnter) {
+          ref.current.addEventListener("dragenter", onDragEnter);
+        }
+        if (onDragLeave) {
+          ref.current.addEventListener("dragleave", onDragLeave);
+        }
+        if (droppable && onDrop) {
+          ref.current.addEventListener("drop", onDrop);
+        }
+        if (onDragOverHandler) {
+          ref.current.addEventListener("dragover", onDragOverHandler);
+        }
+      }
+    };
+  }, [onStart, onDragging, onDrop, ref.current]);
+};
